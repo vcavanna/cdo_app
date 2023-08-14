c48e662dba8798873873d50c6abd897d8683c67b
vincentCav <vin100.cav@gmail.com>
2023-08-13 18:15:13 -0700

    Added api endpoint: people.create

--- people.py
+++ people.py
@@ -1,6 +1,7 @@
 # people.py
 
 from datetime import datetime
+from flask import abort
 
 def get_timestamp():
     return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
@@ -25,3 +26,19 @@ PEOPLE = {
 
 def read_all():
     return list(PEOPLE.values())
+
+def create(person):
+    lname = person.get("lname")
+    fname = person.get("fname", "")
+    if lname and lname not in PEOPLE:
+        PEOPLE[lname] = {
+            "lname": lname,
+            "fname": fname,
+            "timestamp": get_timestamp(),
+        }
+        return PEOPLE[lname], 201
+    else:
+        abort(
+            406,
+            f"Person with last name {lname} already exists",
+        )
